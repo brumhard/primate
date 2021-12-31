1dc3cafc3fbf47bd77200ae50b24dd6f22a4999a
Tobias Brumhard <code@brumhard.com>
2021-12-31 15:27:56 +0100

    Add button to repo card, resize children
    Signed-off-by: Tobias Brumhard <code@brumhard.com>

--- app/lib/ui/repo_card.dart
+++ app/lib/ui/repo_card.dart
@@ -1,6 +1,7 @@
 import 'package:app/services/pr.dart';
 import 'package:app/ui/pr_card.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class RepositoryCard extends StatelessWidget {
   final Repository repo;
@@ -9,6 +10,7 @@ class RepositoryCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
+      color: const Color(0xffedf2f7),
       elevation: 2.0,
       child: Padding(
         padding: const EdgeInsets.all(8.0),
@@ -17,10 +19,28 @@ class RepositoryCard extends StatelessWidget {
           children: [
             Padding(
               padding: const EdgeInsets.all(8.0),
-              child: SelectableText(repo.name,
-                  style: Theme.of(context).textTheme.headline5),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Flexible(
+                    child: Text(repo.name,
+                        overflow: TextOverflow.ellipsis,
+                        style: Theme.of(context).textTheme.headline5),
+                  ),
+                  IconButton(
+                      onPressed: () {},
+                      icon: const FaIcon(FontAwesomeIcons.github))
+                ],
+              ),
             ),
-            ...repo.pullrequests.map((pr) => PRCard(pr: pr)).toList()
+            ...repo.pullrequests
+                .map((pr) => Row(
+                      children: [
+                        Flexible(flex: 1, child: Container()),
+                        Flexible(flex: 4, child: PRCard(pr: pr)),
+                      ],
+                    ))
+                .toList()
           ],
         ),
       ),
